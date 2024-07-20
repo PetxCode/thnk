5d0e3ae063f57fe56ca7bee2cf052b964bed3aad
PetxCode <brighterdayscodelab@gmail.com>
2024-07-20 23:54:10 +0100

    best

---
+++ app/best/feature/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <main>Building this awesome The Best feature!!!</main>
+
+      <br />
+      <br />
+      <br />
+
+      <div>This is a new try!</div>
+    </div>
+  );
+};
+
+export default page;

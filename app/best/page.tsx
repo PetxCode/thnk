7f05d7454eaba8488095cdb0d11cff437e797b9d
Bukolla <gbemibukky@gmail.com>
2024-07-20 22:51:58 +0100

    just a sec push

--- app/best/page.tsx
+++ app/best/page.tsx
@@ -4,6 +4,12 @@ const page = () => {
   return (
     <div>
       <main>Building this awesome feature!!!</main>
+
+      <br />
+      <br />
+      <br />
+
+      <div>This is a new try!</div>
     </div>
   );
 };

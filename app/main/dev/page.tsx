651f8178d3e40360b63c5b75e0a83c4622310fd0
Bukolla <gbemibukky@gmail.com>
2024-07-20 23:44:50 +0100

    dev feature

---
+++ app/main/dev/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <main>Building this awesome dev feature!!!</main>
+
+      <br />
+      <br />
+      <br />
+
+      <div>This is a new try!</div>
+    </div>
+  );
+};
+
+export default page;
